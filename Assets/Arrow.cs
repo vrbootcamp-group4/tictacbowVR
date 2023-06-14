6c40425b1eea48e9fb0b703bdfa1eb33955e9e6e
rertyy <nxtms3@gmail.com>
2023-06-14 15:45:00 +0800

    update arrow script

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 10f;
+    public Transform tip;
+
+    private Rigidbody _rigidBody;
+    private bool _inAir = false;
+    private Vector3 _lastPosition = Vector3.zero;
+
+    private void Awake()
     {
-        
+        _rigidBody = GetComponent<Rigidbody>();
+        PullInteraction.PullActionReleased += Release;
+        StopAllCoroutines();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        PullInteraction.PullActionReleased -= Release;
     }
+
+    private void Release(float value)
+    {
+        PullInteraction.PullActionReleased -= Release;
+        gameObject.transform.parent = null;
+        _inAir = true;
+        SetPhysics(true);
+        _rigidBody.AddForce(transform.forward * speed * value, ForceMode.Impulse);
+
+        StartCoroutine(RotateWithVelocity());
+
+        _lastPosition = tip.position;
+    }
+
+    private IEnumerator RotateWithVelocity()
+    {
+        yield return new WaitForFixedUpdate();
+        while (_inAir)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(_rigidBody.velocity, transform.up);
+            transform.rotation = newRotation;
+            yield return null;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (_inAir)
+        {
+            CheckCollision();
+            _lastPosition = tip.position;
+        }
+    }
+
+    private void CheckCollision()
+    {
+        if (Physics.Linecast(_lastPosition, tip.position, out RaycastHit hitInfo))
+        {
+            if (hitInfo.transform.gameObject.layer != 8)
+            {
+                if (hitInfo.transform.TryGetComponent(out Rigidbody body))
+                {
+                    _rigidBody.interpolation = RigidbodyInterpolation.None;
+                    transform.parent = hitInfo.transform;
+                    body.AddForce(_rigidBody.velocity, ForceMode.Impulse);
+                }
+                Stop();
+            }
+        }
+    }
+
+    private void Stop()
+    {
+        _inAir = false;
+        SetPhysics(false);
+    }
+
+    private void SetPhysics(bool usePhysics)
+    {
+        _rigidBody.useGravity = usePhysics;
+        _rigidBody.isKinematic = !usePhysics;
+
+    }
+
 }
