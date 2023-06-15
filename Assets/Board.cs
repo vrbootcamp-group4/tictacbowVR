171e7babb24b357418ed987dd1241b277c26c982
Elijah Ku <elijahku716@gmail.com>
2023-06-15 17:14:50 +0800

    Update board to communicate with GameManager

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -15,11 +15,9 @@ public class Board : MonoBehaviour
     public Owner currOwner;
     public Owner currPlayer;
     public TextMeshProUGUI display;
-    public int id;
     public int currVal = 0;
 
     // Start is called before the first frame update
-    /*
     void Awake()
     {
         GameManager.OnGameStateChanged += GameManager_OnGameStateChanged;
@@ -27,26 +25,29 @@ public class Board : MonoBehaviour
 
     private void GameManager_OnGameStateChanged(GameManager.GameState state)
     {
-        if (state == GameManager.GameState.Player1Active)
+        if (state == GameManager.GameState.Player1Turn)
         {
             currPlayer = Owner.Red;
         }
 
-        if (state == GameManager.GameState.Player2Active)
+        if (state == GameManager.GameState.Player2Turn)
         {
             currPlayer = Owner.Blue;
         }
     }
     
-    */
     public void UpdateOwner(int newValue)
     {
         if (newValue > currVal) 
         {
             currVal = newValue;
-            currOwner = currPlayer;
-            // Update the owner status by sending a command to the gameManager
-            display.text = currVal.ToString();
+            if (currOwner != currPlayer) 
+            {
+                currOwner = currPlayer;
+                // Update the owner status by sending a command to the gameManager
+                GameManager.Instance.UpdateGameState(GameManager.GameState.Calculate);
+                GameManager.Instance.SetSelectedBoard(this.gameObject.name);
+            }
         }
 
     }
