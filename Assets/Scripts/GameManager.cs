48ecb77b8ed09e316fd285dcc6a434411cc51bb6
Guo Ji Xuan <S10222055@connect.np.edu.sg>
2023-06-15 23:55:24 +0800

    Add Tic Tac Toe Code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,13 +16,18 @@ public class GameManager : MonoBehaviour
 
     public TextMeshProUGUI currentPlayerText;
     public bool player1Active;
+    public bool player1Wins;
     public bool player2Active;
+    public bool player2Wins;
     public Material handleMat;
     public bool arrowIsReleased;
     public float currTime;
 
     private string selectedBoardName;
     public GameObject selectedBoard;
+
+    private string[] board;
+    private bool gameFinished; // Flag to indicate if the game has finished
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -37,6 +42,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        board = new string[9];
+        gameFinished = false;
         UpdateGameState(GameState.SelectMode);
     }
 
@@ -61,41 +68,13 @@ public class GameManager : MonoBehaviour
                 break;
             case GameState.GameOver:
                 break;
-            case GameState.EndGame:
+            case GameState.ExitGame:
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
         }
 
         OnGameStateChanged?.Invoke(newState);
-    }
-
-    public void SetSelectedBoard(string name)
-    {
-        selectedBoardName = name;
-        CalculateWinState();
-    }
-
-    private void CalculateWinState()
-    {
-        for (int i = 0; i < targetBoardArray.Length; i++)
-        {
-            if (targetBoardArray[i].name == selectedBoardName)
-            {
-<<<<<<< Updated upstream
-                selectedBoard = targetBoardArray[i];
-=======
-                if (selectedBoard.GetComponent<Board>().currOwner == Board.Owner.Red)
-                {
-                    board[i + 1] = '1';
-                }
-                if (selectedBoard.GetComponent<Board>().currOwner == Board.Owner.Blue)
-                {
-                    board[i + 1] = '2';
-                }
->>>>>>> Stashed changes
-            }
-        }
     }
 
     private void SetPlayer1Turn()
@@ -137,9 +116,91 @@ public class GameManager : MonoBehaviour
                 resetTimer();
                 arrowIsReleased = false;
                 // Switch turn
-                UpdateGameState(GameState.Calculate);
+                if (player1Active)
+                {
+                    UpdateGameState(GameState.Player2Turn);
+                }
+                else if (player2Active)
+                {
+                    UpdateGameState(GameState.Player1Turn);
+                }
             }
         }  
+    }
+
+    public void SetSelectedBoard(string name)
+    {
+        selectedBoardName = name;
+        CalculateWinState();
+    }
+
+    private void CalculateWinState()
+    {
+        for (int i = 0; i < targetBoardArray.Length; i++)
+        {
+            if (targetBoardArray[i].name == selectedBoardName)
+            {
+                selectedBoard = targetBoardArray[i];
+                if (selectedBoard.GetComponent<Board>().currOwner == Board.Owner.Red)
+                {
+                    board[i] = "1";
+                    if (CheckWinCondition("1"))
+                    {
+                        //winText.text = "Player " + currentPlayer + " wins!";
+                        //winText.gameObject.SetActive(true);
+                        gameFinished = true;
+                        player1Wins = true;
+                        UpdateGameState(GameState.GameOver);
+                    }
+                }
+                if (selectedBoard.GetComponent<Board>().currOwner == Board.Owner.Blue)
+                {
+                    board[i] = "2";
+                    if (CheckWinCondition("2"))
+                    {
+                        //winText.text = "Player " + currentPlayer + " wins!";
+                        //winText.gameObject.SetActive(true);
+                        gameFinished = true;
+                        player2Wins = true;
+                        UpdateGameState(GameState.GameOver);
+                    }
+                }
+            }
+        }
+    }
+
+    bool CheckWinCondition(string player)
+    {
+        // Check rows
+        for (int i = 0; i < 9; i += 3)
+        {
+            if (board[i] == player && board[i + 1] == player && board[i + 2] == player)
+                return true;
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i] == player && board[i + 3] == player && board[i + 6] == player)
+                return true;
+        }
+
+        // Check diagonals
+        if ((board[0] == player && board[4] == player && board[8] == player) ||
+            (board[2] == player && board[4] == player && board[6] == player))
+            return true;
+
+        return false;
+    }
+
+    bool IsBoardFull()
+    {
+        foreach (string value in board)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+        }
+        return true;
     }
 
     public void resetTimer() 
@@ -155,6 +216,6 @@ public class GameManager : MonoBehaviour
         Calculate,
         Restart,
         GameOver, 
-        EndGame,
+        ExitGame,
     }
 }
