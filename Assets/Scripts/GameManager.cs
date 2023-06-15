3a573e0886dfddc0deb646580ae539a770ab3774
Guo Ji Xuan <S10222055@connect.np.edu.sg>
2023-06-15 17:51:07 +0800

    Added Tic Tac Toe code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,9 +24,11 @@ public class GameManager : MonoBehaviour
     private string selectedBoardName;
     public GameObject selectedBoard;
 
+    private char[] board;
+
     private void OnCollisionEnter(Collision collision)
     {
-        
+
     }
 
     void Awake()
@@ -55,7 +57,6 @@ public class GameManager : MonoBehaviour
                 SetPlayer2Turn();
                 break;
             case GameState.Calculate:
-                CalculateWinState();
                 break;
             case GameState.Restart:
                 break;
@@ -73,18 +74,61 @@ public class GameManager : MonoBehaviour
     public void SetSelectedBoard(string name)
     {
         selectedBoardName = name;
-        CalculateWinState();
+        SetSelectedBoardObject();
     }
 
-    private void CalculateWinState()
+    private void SetSelectedBoardObject()
     {
         for (int i = 0; i < targetBoardArray.Length; i++)
         {
             if (targetBoardArray[i].name == selectedBoardName)
             {
                 selectedBoard = targetBoardArray[i];
+                CheckBoardOwner();
             }
         }
+    }
+
+    private void CheckBoardOwner()
+    {
+        if (selectedBoard.GetComponent<Board>().currOwner == Board.Owner.Red)
+        {
+
+        }
+    }
+
+    private bool CheckWinCondition()
+    {
+        // Check rows
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i * 3] != ' ' && board[i * 3] == board[i * 3 + 1] && board[i * 3] == board[i * 3 + 2])
+            {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i] != ' ' && board[i] == board[i + 3] && board[i] == board[i + 6])
+            {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0] != ' ' && board[0] == board[4] && board[0] == board[8])
+        {
+            return true;
+        }
+
+        if (board[2] != ' ' && board[2] == board[4] && board[2] == board[6])
+        {
+            return true;
+        }
+
+        return false;
     }
 
     private void SetPlayer1Turn()
