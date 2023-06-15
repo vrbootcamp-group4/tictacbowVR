dda7130a26dc627a07c91da065f9a22418d227b5
Elijah Ku <elijahku716@gmail.com>
2023-06-15 11:12:49 +0800

    Disable unique hand pose

--- Assets/GrabHandPose.cs
+++ Assets/GrabHandPose.cs
@@ -6,6 +6,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class GrabHandPose : MonoBehaviour
 {
     public HandDataComponent leftHandPose;
+    bool setPose;
+    private HandDataComponent handData;
+    private Vector3 startingHandPosition;
+    private Vector3 finalHandPosition;
+    private Quaternion startingHandRotation;
+    private Quaternion finalHandRotation;
+
+    private Quaternion[] startingFingerRotations;
+    private Quaternion[] finalFingerRotations;
     // Start is called before the first frame update
     void Start() 
     {
@@ -16,14 +25,52 @@ public class GrabHandPose : MonoBehaviour
         leftHandPose.gameObject.SetActive(false);
     }
 
+    void Update()
+    {
+        if (setPose && handData != null)
+        {
+            handData.anim.enabled = false;
+            SetHandDataValues(handData, leftHandPose);
+            SetHandData(handData, finalHandPosition, finalHandRotation, finalFingerRotations);
+        }
+    }
+
     public void SetupPose(BaseInteractionEventArgs arg) 
     {
         if (arg.interactorObject is XRDirectInteractor) 
         {
-            HandDataComponent handData = arg.interactorObject.transform.GetComponentInChildren<HandDataComponent>();
+            handData = arg.interactorObject.transform.GetComponent<HandDataComponent>();
             
-            Debug.Log(handData == null);
-            handData.anim.enabled = false;
+            setPose = true;
+        }
+    }
+
+    public void SetHandDataValues(HandDataComponent h1, HandDataComponent h2)
+    {
+        startingHandPosition = h1.root.localPosition;
+        finalHandPosition = h2.root.localPosition;
+
+        startingHandRotation = h1.root.localRotation;
+        finalHandRotation = h2.root.localRotation;
+
+        startingFingerRotations = new Quaternion[h1.fingies.Length];
+        finalFingerRotations = new Quaternion[h2.fingies.Length];
+
+        for (int i = 0; i < h1.fingies.Length; i++)
+        {
+            startingFingerRotations[i] = h1.fingies[i].localRotation;
+            finalFingerRotations[i] = h2.fingies[i].localRotation;
+        }
+    }
+
+    public void SetHandData(HandDataComponent h, Vector3 newPosition, Quaternion newRotation, Quaternion[] newBonesRotation) 
+    {
+        h.root.localPosition = newPosition;
+        h.root.localRotation = newRotation;
+
+        for (int i = 0; i < newBonesRotation.Length; i++) 
+        {
+            h.fingies[i].localRotation = newBonesRotation[i];
         }
     }
 }
